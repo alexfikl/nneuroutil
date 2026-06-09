ea54469102007c14270549afbda5275a92c1a7be
Alexandru Fikl <alexfikl@gmail.com>
2026-06-09 09:47:48 +0300

    chore: remove empty file

--- src/nneuroutil/architecture.py
+++
@@ -1,8 +0,0 @@
-# SPDX-FileCopyrightText: 2026 Alexandru Fikl <alexfikl@gmail.com>
-# SPDX-License-Identifier: MIT
-
-from __future__ import annotations
-
-from nneuroutil.helpers import module_logger
-
-log = module_logger(__name__)
